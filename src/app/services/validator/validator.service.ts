526ddc3a384976e2806fbc46e3a478de5a24ec76
Aditya Bhave <adi007me@gmail.com>
2019-11-06 00:47:58 +0530

    Updated APK and Service

--- src/app/services/validator/validator.service.ts
+++ src/app/services/validator/validator.service.ts
@@ -2,42 +2,48 @@ import { Injectable } from '@angular/core';
 import { HTTP } from '@ionic-native/http/ngx';
 import { SmartScanForm } from '../../models/SmartScanForm'
 import { Platform } from '@ionic/angular';
+import { ValidationResultItem } from 'src/app/models/ValidationResults';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ValidatorService {
-  constructor(private http: HTTP, private platform: Platform) { }
+  constructor(private http: HTTP, private platform: Platform, private toastController: ToastController) { }
 
   validateForm(smartScanForm: SmartScanForm) {
-    const url = 'https://13.75.106.59:443/syntbots-ai/humana/ai/humanaextract1';
+    const url = 'http://13.75.106.59/syntbots-ai/humana/ai/humanaextract1';
     let formData:FormData = new FormData();
 
     formData.append('file', this.B64toBlob(smartScanForm.FormPages[0].imageData), 'file.name' + Math.random() + '.jpg');
 
-    return new Promise((resolve, reject) => {
-      fetch(url, {
-          body: formData,
-          headers: {
-            'Content-Type': 'multipart/form-data',
-            'Accept': 'application/json'
-          },
-          method: 'POST'
-      })
-      .then(response => {
-        console.log(response);
+    if (false && this.platform.is('cordova')) {
+      return new Promise((resolve, reject) => {
+        this.http.sendRequest(url,{ method: "post", data: formData }).then(value => {
+          resolve(this.extractValidationResult(value.data));
+        }).catch(error => {
+          this.handleError(error);
 
-        return response.json()
-      })
-      .then(success => {
-          console.log(success);
+          reject(error);
+        });
+      });
+    } else {
+      return new Promise((resolve, reject) => {
+        fetch(url, {
+            body: formData,
+            method: 'POST'
+        })
+        .then(response => {
+            return response.json()
+        })
+        .then(success => {
+            return resolve(this.extractValidationResult(success));
+        })
+        .catch(error => {
+            this.handleError(error);
 
-          return resolve(success.response.file_response)
-      })
-      .catch(error => {
-          console.log(error)
-
-          return reject(error);
+            return reject(error);
+        });
       });
 
       //Mock Data
@@ -54,7 +60,7 @@ export class ValidatorService {
 
       //   resolve(mockResponse)
       // }, 5000);
-    });
+    }
   }
 
   private B64toBlob(dataURI) {
@@ -67,5 +73,42 @@ export class ValidatorService {
     }
 
     return new Blob([ab], { type: 'image/jpeg' });
+  }
+
+  private extractValidationResult(data) {
+    let validationResults: ValidationResultItem[] = new Array();
+    const response = data.response[0].file_response;
+    const keys = Object.keys(response);
+
+    (async () => {
+      const toast = await this.toastController.create({
+        message: 'Service Call Successful: ' + JSON.stringify(response),
+        duration: 10000,
+        showCloseButton: true
+      });
+      toast.present();
+    })();
+
+    keys.forEach(key => {
+      const validationResultItem: ValidationResultItem = {
+        key: key,
+        value: response[key]
+      };
+
+      validationResults.push(validationResultItem);
+    });
+
+    return validationResults;
+  }
+
+  private handleError(error) {
+    (async () => {
+      const toast = await this.toastController.create({
+        message: 'Service Call Failed : ' + error,
+        duration: 10000,
+        showCloseButton: true
+      });
+      toast.present();
+    })();
   }
 }
